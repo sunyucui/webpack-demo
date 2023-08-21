e4dd80476b7deb13d8d750104db4ed19131703f5
sunyucui <yucui.sun@weimob.com>
2023-08-21 18:30:28 +0800

    fix

--- src/component/footer.js
+++ src/component/footer.js
@@ -2,5 +2,6 @@ function Footer(dom) {
     const footer = document.createElement('div')
     footer.innerText = 'footer';
     dom.appendChild(footer);
+    console.log('footer')
 }
 export default Footer
